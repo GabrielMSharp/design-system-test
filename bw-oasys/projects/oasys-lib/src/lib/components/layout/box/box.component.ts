00e89eba66f0d6bfde7ac66a0e9d89a71683768f
Gabriel Sharp <gabriel.m.sharp@gmail.com>
2022-04-04 09:59:17 +0100

    basic box sizing classes

--- bw-oasys/projects/oasys-lib/src/lib/components/layout/box/box.component.ts
+++ bw-oasys/projects/oasys-lib/src/lib/components/layout/box/box.component.ts
@@ -17,12 +17,32 @@ export class LayoutBoxComponent implements OnInit {
 
   @Input() padding?: BoxPaddings = 'default';
 
+  @Input() paddingX?: BoxPaddings;
+  @Input() paddingY?: BoxPaddings;
+ 
+  @Input() paddingTop?: BoxPaddings;
+  @Input() paddingRight?: BoxPaddings;
+  @Input() paddingBottom?: BoxPaddings;
+  @Input() paddingLeft?: BoxPaddings;
+
   constructor() { }
 
   ngOnInit(): void {
-    this.boxLayoutClasses = [
-      `ui-layout-box-${this.padding}`
+    
+    this.boxLayoutClasses = [      
+      this.paddingY ? `ui-layout-box-y-${this.paddingY}` : '',
+      this.paddingX ? `ui-layout-box-x-${this.paddingX}` : '',
+
+      this.paddingTop ? `ui-layout-box-top-${this.paddingTop}` : '',
+      this.paddingRight ? `ui-layout-box-right-${this.paddingRight}` : '',
+      this.paddingBottom ? `ui-layout-box-bottom-${this.paddingBottom}` : '',
+      this.paddingLeft ? `ui-layout-box-leftx-${this.paddingLeft}` : '',
     ].join(' ');
+
+    if(!this.boxLayoutClasses.length) {
+      this.boxLayoutClasses.concat(` ui-layout-box-${this.padding}`);
+    }
+
   }
 
 }
