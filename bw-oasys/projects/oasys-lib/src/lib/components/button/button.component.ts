d4e0152b9b8e63b3a5dd763cbfb206bd8fb2b8fe
Gabriel Sharp <gabriel.m.sharp@gmail.com>
2022-06-07 17:30:18 +0100

    omg

--- bw-oasys/projects/oasys-lib/src/lib/components/button/button.component.ts
+++ bw-oasys/projects/oasys-lib/src/lib/components/button/button.component.ts
@@ -1,57 +1,57 @@
 import { Component, Input, OnInit, Output, ViewEncapsulation, EventEmitter, ChangeDetectionStrategy } from '@angular/core';
-// import { TokenService } from '../../services/token.service';
-// import { IconNames, IconContext } from '../icon/icon';
-// import { TextTransform } from '../text/text';
+import { TokenService } from '../../services/token.service';
+import { IconNames, IconContext } from '../icon/icon';
+import { TextTransform } from '../text/text';
 
 @Component({
   selector:'ui-button',
   templateUrl: './button.component.html',
-  styleUrls: ['./button.component.scss']
+  styleUrls: ['./button.component.scss'],
+  encapsulation: ViewEncapsulation.None,
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class ButtonComponent implements OnInit {
 
-  buttonText: string = 'fff'
-
   // // Button Content
-  // @Input() buttonText?: string;
-  // @Input() buttonIcon?: IconNames;
-  // @Input() buttonIconPlacement: IconContext = 'leading';
+  @Input() buttonText?: string;
+  @Input() buttonIcon?: IconNames;
+  @Input() buttonIconPlacement: IconContext = 'leading';
 
-  // // Button Stylings
-  // @Input() buttonSize: 'small'|'large' = 'large';
-  // @Input() buttonType: 'primary'|'primary-2'|'primary-3'|'secondary'|'secondary-2' = 'primary';
+  // Button Stylings
+  @Input() buttonSize: 'small'|'large' = 'large';
+  @Input() buttonType: 'primary'|'primary-2'|'primary-3'|'secondary'|'secondary-2' = 'primary';
   
-  // // Button Actions
-  // @Input() href: string = '';
-  // @Output() buttonClick: EventEmitter<void> = new EventEmitter();
+  // Button Actions
+  @Input() href: string = '';
+  @Output() buttonClick: EventEmitter<void> = new EventEmitter();
 
-  // buttonClasses: string = '';
-  // iconContext: IconContext = 'none';
-  // textTransform!: TextTransform;
+  buttonClasses: string = '';
+  iconContext: IconContext = 'none';
+  textTransform!: TextTransform;
 
-  // constructor(private tokenService: TokenService) { }
+  constructor(private tokenService: TokenService) { }
 
-  // clickButton(): void {
-  //   console.log('button was clicked');
-  //   this.buttonClick.emit();
-  // }
+  clickButton(): void {
+    console.log('button was clicked');
+    this.buttonClick.emit();
+  }
 
   ngOnInit(): void {
-  //   this.buttonClasses = [
-  //     `type-${this.buttonType}`,
-  //     `size-${this.buttonSize}`,
-  //     `${this.buttonIcon ? 'button--has-icon': ''}`,
-  //     `${this.buttonText && this.buttonIcon ? 'button--icon--'+this.buttonIconPlacement : ''}`,
-  //     `${!this.buttonText && this.buttonIcon ? 'button--icon--only' : ''}`
-  //   ].join(' ');
+    this.buttonClasses = [
+      `type-${this.buttonType}`,
+      `size-${this.buttonSize}`,
+      `${this.buttonIcon ? 'button--has-icon': ''}`,
+      `${this.buttonText && this.buttonIcon ? 'button--icon--'+this.buttonIconPlacement : ''}`,
+      `${!this.buttonText && this.buttonIcon ? 'button--icon--only' : ''}`
+    ].join(' ');
 
-  //   if(this.buttonIcon) {
-  //     this.iconContext = this.buttonText ? this.buttonIconPlacement : 'iconOnly';
-  //   }
+    if(this.buttonIcon) {
+      this.iconContext = this.buttonText ? this.buttonIconPlacement : 'iconOnly';
+    }
 
-  //   this.textTransform = this.tokenService.getTokenValue(
-  //     `--style-button-${this.buttonSize}-text-transform`
-  //   ) as TextTransform;
+    this.textTransform = this.tokenService.getTokenValue(
+      `--style-button-${this.buttonSize}-text-transform`
+    ) as TextTransform;
   }
 
 }
--- bw-oasys/stories/Test.stories.ts
+++ bw-oasys/stories/Test.stories.ts
@@ -1,7 +1,4 @@
-// also exported from '@storybook/angular' if you can deal with breaking changes in 6.1
-// import { Story, Meta } from '@storybook/angular/types-6-0';
 import { componentWrapperDecorator, Meta, moduleMetadata } from '@storybook/angular';
-import { ButtonComponent } from 'projects/oasys-lib/src/public-api';
 import { TestComponent } from './test.component';
 
 // More on default export: https://storybook.js.org/docs/angular/writing-stories/introduction#default-export
@@ -9,10 +6,6 @@ export default {
   title: 'Example/Test',
   component: TestComponent,
   decorators: [
-    moduleMetadata({
-      //👇 Imports both components to allow component composition with Storybook
-      declarations: [ButtonComponent],      
-    }),
     //👇 Wraps our stories with a decorator
     componentWrapperDecorator(story => `<div style="margin: 3em">${story}</div>`),
   ],
