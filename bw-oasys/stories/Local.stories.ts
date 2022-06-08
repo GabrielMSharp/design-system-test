8499e3e0a816f750a6dda359208813eaea24865e
Gabriel Sharp <gabriel.m.sharp@gmail.com>
2022-06-08 10:34:22 +0100

    local button test

---
+++ bw-oasys/stories/Local.stories.ts
@@ -0,0 +1,23 @@
+// also exported from '@storybook/angular' if you can deal with breaking changes in 6.1
+import { Story, Meta, moduleMetadata } from '@storybook/angular';
+import { ButtonComponent } from './button.component';
+
+export default {
+  title: 'Example/ButtonLocal',
+  component: ButtonComponent,
+  decorators: [
+    moduleMetadata({
+      declarations: [ButtonComponent]
+    })
+  ]
+} as Meta;
+
+const Template: Story<ButtonComponent> = (args: ButtonComponent) => ({
+  props: args,
+});
+
+export const Primary = Template.bind({});
+Primary.args = {
+  buttonType: 'primary',
+  buttonText: 'Default',
+};
