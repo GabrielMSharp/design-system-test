b9858396f21a35cdc4dc11ebb0b10d1e06cf780f
Gabriel Sharp <gabriel.m.sharp@gmail.com>
2022-06-21 10:57:12 +0100

    clean up file

--- bw-oasys/projects/oasys-lib/style-dictionary/merge-tokens.js
+++ bw-oasys/projects/oasys-lib/style-dictionary/merge-tokens.js
@@ -1,35 +1,20 @@
 const fs = require('fs');
-const path = require('path');
-const glob = require('path');
 
 console.log("Merge token files for Figma...");
 
-// PROCESS THE DESIGN TOKENS FOR THE DIFFEREN BRANDS
+console.log("\n==============================================");
 
+// Get global, bloomon and bloomandwild tokens
+const globalTokens = JSON.parse(fs.readFileSync("figma-tokens/tokens.json"))['global'];
+const bloomonTokens = JSON.parse(fs.readFileSync("figma-tokens/tokens-bloomon.json"))['bloomon'];
+const bloomandwildTokens = JSON.parse(fs.readFileSync("figma-tokens/tokens-bloomandwild.json"))['bloomandwild'];
 
-// ["bloomandwild", "bloomon"].map(function (brand) {
-    console.log("\n==============================================");
+// Merge tokens into one object
+const mergedTokens = {
+  'global': globalTokens,
+  'bloomon': bloomonTokens,
+  'bloomandwild': bloomandwildTokens
+}
 
-    const globalTokens = JSON.parse(fs.readFileSync("figma-tokens/tokens.json"))['global'];
-    const bloomonTokens = JSON.parse(fs.readFileSync("figma-tokens/tokens-bloomon.json"))['bloomon'];
-    const bloomandwildTokens = JSON.parse(fs.readFileSync("figma-tokens/tokens-bloomandwild.json"))['bloomandwild'];
-
-    const mergedTokens = {
-      'global': globalTokens,
-      'bloomon': bloomonTokens,
-      'bloomandwild': bloomandwildTokens
-    }
-    fs.writeFileSync('figma-tokens/tokens.json', JSON.stringify(mergedTokens, null, 2), 'utf8');
-    // , function(err, file) {
-      // console.log(tokens.toString());
-    // });
-
-    // let pugFiles = glob.sync(`${base}src/**/*.pug`);
-
-    // let contents = fs.readFileSync(filePath, 'utf8');
-    // contents = contents.replace(` ${item}`, ` !{t('${key}')}`);
-    // fs.writeFileSync('./new_translations.json', JSON.stringify(translationKeys, null, 2));
-
-
-
-// });
+// Write back to the main json that Figma is connected to via Figma Tokens plugin
+fs.writeFileSync('figma-tokens/tokens.json', JSON.stringify(mergedTokens, null, 2), 'utf8');
