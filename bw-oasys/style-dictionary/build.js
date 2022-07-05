4cdd3bdcb7bf5a3ddae9df32b9b8ebfd1cefd5ec
Gabriel Sharp <gabriel.m.sharp@gmail.com>
2022-07-05 14:47:43 +0100

    attempt at alpha calc

--- bw-oasys/style-dictionary/build.js
+++ bw-oasys/style-dictionary/build.js
@@ -1,10 +1,30 @@
 console.log("Running Bloom & Wild token building");
 
-// const { transform } = require('@divriots/style-dictionary-to-figma');
-// import { transform } from '@divriots/style-dictionary-to-figma';
 const { trimValue,  trimName,  useRefValue,  markTokenset } = require('@divriots/style-dictionary-to-figma');
 const StyleDictionaryPackage = require("style-dictionary");
 const { fileHeader, formattedVariables } = StyleDictionaryPackage.formatHelpers;
+const chroma = require('chroma-js');
+
+const colorTransform = (token) => {
+  const { value, modify = [] } = token;
+  let color = chroma(value);
+
+  // iterate over the modify array (see tokens/color.json)
+  // and apply each modification in order
+  modify.forEach(({ type, amount }) => {
+    // modifier type must match a method name in chromajs
+    // https://gka.github.io/chroma.js/
+    // chroma methods can be chained, so each time we override the color variable
+    // we can still call other chroma methods, similar to
+    // chroma(value).brighten(1).darken(1).hex();
+    console.log("Hi", color);
+    console.log("HELLO", type, " ", amount);
+    color = color[type](amount);
+  });
+
+
+  return color.hex();
+}
 
 // HAVE THE STYLE DICTIONARY CONFIG DYNAMICALLY GENERATED
 
@@ -62,9 +82,25 @@ function getStyleDictionaryConfig(brand) {
         `./style-dictionary/tokens/global/**/*.json`,
         `./style-dictionary/tokens/${brand}/**/**/*.json`
     ],
+    transform: {
+      colorTransform: {
+        type: `value`,
+        // only transforms that have transitive: true will be applied to tokens
+        // that alias/reference other tokens
+        transitive: true,
+        matcher: (token) => token.modify,
+        transformer: colorTransform
+      },
+
+      // For backwards compatibility, all built-in transforms are not transitive
+      // by default. This will make the 'color/css' transform transitive
+      'color/css': Object.assign({}, StyleDictionaryPackage.transform[`color/css`], {
+        transitive: true
+      }),
+    },
     platforms: {
       brandTokensCss: {
-        transformGroup: "css",
+        transforms: [`attribute/cti`, `name/cti/kebab`, `colorTransform`, `color/css`],
         files: [
           {
             destination: `./projects/oasys-lib/src/assets/${brand}/variables.css`,
@@ -81,7 +117,7 @@ function getStyleDictionaryConfig(brand) {
         ],
       },
       globalTokensCss: {
-        transformGroup: "css",
+        transforms: [`attribute/cti`, `name/cti/kebab`, `colorTransform`, `color/css`],
         files: [
           {
             destination: `./projects/oasys-lib/src/assets/global/variables.css`,
@@ -97,7 +133,7 @@ function getStyleDictionaryConfig(brand) {
         ],
       },
       figmaJsonGlobal: {
-        transforms: ["attribute/cti", "name/cti/kebab", "color/hex", "size/remToPx"],
+        transforms: ["attribute/cti", "name/cti/kebab", "color/hex", "size/remToPx", 'colorTransform'],
         buildPath: './style-dictionary/figma-tokens/',
         basePxFontSize: '10',
         files: [
